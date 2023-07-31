9d6dd9e637f5d0933b236739b23ee4184e1e31a0
Никита Матвеев <nik.nikita.matveev.1997@gmail.com>
2023-07-31 22:40:54 +0500

    Create empty ts application

---
+++ backend/index.ts
@@ -0,0 +1,9 @@
+type message = string | null;
+
+function testTS(str : message) : void{
+    console.log(str);
+}
+
+let variable : message = "ts succes build and start and daemon 2"
+
+testTS(variable);
---
+++ backend/ts_build/index.js
@@ -0,0 +1,7 @@
+"use strict";
+function testTS(str) {
+    console.log(str);
+}
+let variable = "ts succes build and start";
+testTS(variable);
+//# sourceMappingURL=index.js.map
