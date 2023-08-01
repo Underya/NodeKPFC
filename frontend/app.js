6f1bfebd293ecd245bf393d3878cc689ba4426f1
Никита Матвеев <nik.nikita.matveev.1997@gmail.com>
2023-08-02 00:24:42 +0500

    WIP

---
+++ frontend/app.js
@@ -0,0 +1,23 @@
+const http = require("http");
+const fs = require("fs");
+const port = 4000;   
+
+http.createServer(function(request, response){
+
+    fs.readFile("index.html", function(error, data){
+             
+        console.log("this");
+        if(error){
+                   
+            response.statusCode = 404;
+            response.end("Resourse not found!");
+        }   
+        else{
+            console.log("else!");
+            response.end(data);
+        }
+    });
+
+}).listen(port, function(){
+    console.log(`Server started at ${port}`);
+});
