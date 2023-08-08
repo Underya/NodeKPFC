8ca72447656dbcfa76f249838399b6b8204eaea2
Matveev Nikita <67943275+Underya@users.noreply.github.com>
2023-08-09 00:26:57 +0500

    add typescriprt (#2)
    Co-authored-by: Nikita Matveev <matveev@mindbox.cloud>

--- frontend/app.jsx
+++ frontend/app.jsx
@@ -1,8 +1,10 @@
 import ReactDOM from "react-dom/client";
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { testTsClass } from "./test";
 
 
+let message = new testTsClass().GetMessage();
 ReactDOM.createRoot(
     document.getElementById("app")
 )
@@ -10,6 +12,7 @@ ReactDOM.createRoot(
     <Router>
         <div>
             <Routes>
+                <Route path="/ts" element={<div>{message}</div>} />
                 <Route path="*" element={<div>Hello World!</div>} />
             </Routes>
         </div>
